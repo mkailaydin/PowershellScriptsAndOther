4b27b24a7aade07fa36072722317d08b7afc130a
Mikail Aydın <mikail.aydin@windowslive.com>
2023-09-12 12:20:17 +0300

    Create CreateBulkOU.ps1

---
+++ AD/CreateBulkOU.ps1
@@ -0,0 +1,25 @@
+<#
+=============================================================================================
+Author          = Mikail AYDIN
+Creation Date   = 12.09.2023
+
+============================================================================================
+#>
+
+# Base OU
+$baseOU = "DC=aydin,DC=local"
+
+# OU Names
+$ouNames = @("IT", "Management","Finance","Production","Consultant","Technician")
+
+# Base OU and sub OU's create process
+foreach ($ouName in $ouNames) {
+    $ouPath = "OU=$ouName,$baseOU"
+    New-ADOrganizationalUnit -Name $ouName -Path $baseOU
+    
+    # Create sub OU
+    $subOUs = @("Users", "Comp", "Groups")
+    foreach ($subOUName in $subOUs) {
+        New-ADOrganizationalUnit -Name $subOUName -Path $ouPath
+    }
+}
